12ae865ae7c41edf9374a7b9a603641c3a334bf1
terabyte-256 <maxstone2644@gmail.com>
2025-02-06 21:17:42 -0800

    Refactor GachaCalculator to use static constants and improve probability calculation logic

--- js/gacha-calculator.js
+++ js/gacha-calculator.js
@@ -1,24 +1,26 @@
 export class GachaCalculator {
-    // uses monte carlo
-    
-    static constants = {
+    static constants = Object.freeze({
         FIVE_STAR_CHARACTER_CHANCE: 0.006,
         FIVE_STAR_CONE_CHANCE: 0.008,
-        
         CHARACTER_SOFT_PITY: 74,
         SOFT_PITY_INCREMENT: 0.062,
         CHARACTER_PITY: 90,
-        
         CONE_SOFT_PITY: 64,
         CONE_PITY: 80,
-        
         LIMITED_CHARACTER_CHANCE: 0.5,
         LIMITED_CONE_CHANCE: 0.75,
+    });
 
-    };
-
-
-    calculateProbability(pulls, existing_character_pity = 0, existing_cone_pity = 0, character_copies = 0, cone_copies = 0, character_guaranteed = false, cone_guaranteed = false, num_simulations) {
+    calculateProbability(
+        pulls, 
+        existing_character_pity = 0, 
+        existing_cone_pity = 0, 
+        character_copies = 0, 
+        cone_copies = 0, 
+        character_guaranteed = false, 
+        cone_guaranteed = false, 
+        num_simulations
+        ) {
         
         let successful_sims = 0;
         
@@ -39,16 +41,16 @@ export class GachaCalculator {
                 
                 while (pulls_left > 0 && ((character_success < character_copies) || (character_success == character_copies && character_copies == 0))) {
                     // 5 star prob
-                    let random_value = random.random();
-                    let current_five_star_chance = this.constants.FIVE_STAR_CHARACTER_CHANCE;
+                    let random_value = Math.random();
+                    let current_five_star_chance = GachaCalculator.constants.FIVE_STAR_CHARACTER_CHANCE;
         
                     if (cone_copies > 0 && character_success < character_copies) {
                     // increments if current pity number is 74
-                    current_five_star_chance += this.constants.SOFT_PITY_INCREMENT * Math.max(current_character_pity - this.constants.CHARACTER_SOFT_PITY, 0.0);
+                    current_five_star_chance += GachaCalculator.constants.SOFT_PITY_INCREMENT * Math.max(current_character_pity - GachaCalculator.constants.CHARACTER_SOFT_PITY, 0.0);
         
-                        if (random_value < current_five_star_chance || current_character_pity + 1 == this.constants.CHARACTER_PITY) {
+                        if (random_value < current_five_star_chance || current_character_pity + 1 == GachaCalculator.constants.CHARACTER_PITY) {
                         
-                            if (current_character_guaranteed || random.random() < this.constants.LIMITED_CHARACTER_CHANCE) {
+                            if (current_character_guaranteed || Math.random() < GachaCalculator.constants.LIMITED_CHARACTER_CHANCE) {
                                 character_success += 1;
                                 current_character_guaranteed = false;
                                 current_character_pity = 0;
@@ -61,11 +63,11 @@ export class GachaCalculator {
                         }
                     } else {
                         // Cone prob
-                        current_five_star_chance = this.constants.FIVE_STAR_CONE_CHANCE;
-                        current_five_star_chance += this.constants.SOFT_PITY_INCREMENT * Math.max(current_cone_pity - this.constants.CONE_SOFT_PITY, 0.0);
+                        current_five_star_chance = GachaCalculator.constants.FIVE_STAR_CONE_CHANCE;
+                        current_five_star_chance += GachaCalculator.constants.SOFT_PITY_INCREMENT * Math.max(current_cone_pity - GachaCalculator.constants.CONE_SOFT_PITY, 0.0);
         
-                        if (random_value < current_five_star_chance || current_cone_pity + 1 == this.constants.CONE_PITY) {
-                            if (current_cone_guaranteed || random.random() < this.constants.LIMITED_CONE_CHANCE) {
+                        if (random_value < current_five_star_chance || current_cone_pity + 1 == GachaCalculator.constants.CONE_PITY) {
+                            if (current_cone_guaranteed || Math.random() < GachaCalculator.constants.LIMITED_CONE_CHANCE) {
                                 cone_successes += 1;
                                 current_cone_guaranteed = false;
                                 current_cone_pity = 0;
